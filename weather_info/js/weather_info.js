7dcc9210c6faa34d474d7f990d7451d795fa56b5
unknown <de.waele.stef@gmail.com>
2013-02-23 13:54:00 +0100

    brol verwijderd

--- weather_info/js/weather_info.js
+++ weather_info/js/weather_info.js
@@ -7,17 +7,17 @@ $(document).on('click', '#zoek', function(){
 	$("#block-weather-info-user-custom").fadeOut("fast");
     $.ajax({
         type: "GET",
-        //url: "proxy.php?url=http%3A%2F%2Fweather.yahooapis.com%2Fforecastrss%3Fw%3D" + $("#invoer").val() + "%26u%3Dc",
+        url: "proxy.php?url=http%3A%2F%2Fweather.yahooapis.com%2Fforecastrss%3Fw%3D" + $("#invoer").val() + "%26u%3Dc",
 		//url: "proxy.php?url=http%3A%2F%2Fcoap.me%2Fcoap%3A%2F%2F[2001%3A6a8%3A1d80%3A200%3A%3A2]%2Fobs,
-		url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
-        dataType: "html",
-        success: parseHtml
+		//url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
+        dataType: "xml",
+        success: parseXml
      });
 });
 
-function parseHtml(html)
+function parseXml(xml)
 {
 	$("#weerblok").empty();
-	 $("#weerblok").append($(html).find("span").text());
+	 $("#weerblok").append($(xml).find("description").text());
   $("#block-weather-info-user-custom").fadeIn("slow");
 }
