6af9ba0a84c51a30e588f6b574e55cb1047fbcdc
unknown <wrightkobe@hotmail.com>
2013-03-05 10:44:55 +0100

    Node.js stukken in commentaar, probleem met jQuery opgelost waardoor functionaliteit van toolbar van drupal niet goed meer werkte, code i.v.m. timer toegevoegd

--- temperatuursensor/js/temperatuursensor.js
+++ temperatuursensor/js/temperatuursensor.js
@@ -1,16 +1,18 @@
+(function ($) {
+
 $(document).ready(function(){
 	$('#invoer').val('970013');
 	$('#zoek').click();
-});
-
-$(document).on('click', '#zoek', function(){
-	$("#block-temperatuursensor-user-custom").fadeOut("fast");
-    $.ajax({
-        type: "GET",
-		url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
-        dataType: "html",
-        success: parseHtml
-     });
+	
+	$('#zoek').click(function(){
+		$("#block-temperatuursensor-user-custom").fadeOut("fast");
+		$.ajax({
+			type: "GET",
+			url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
+			dataType: "html",
+			success: parseHtml
+		});
+	});
 });
 
 function parseHtml(html)
@@ -19,3 +21,11 @@ function parseHtml(html)
 	 $("#temperatuurblok").append($(html).find("span").text());
   $("#block-temperatuursensor-user-custom").fadeIn("slow");
 }
+
+Drupal.Nodejs.callbacks.waardeOntvangen = {
+  callback: function (message) {
+    Drupal.nodejs_ajax.runCommands(message);
+  }
+};
+
+})(jQuery);
--- weather_info/js/weather_info.js
+++ weather_info/js/weather_info.js
@@ -1,23 +1,29 @@
+(function ($) {
+
 $(document).ready(function(){
-	//$('#invoer').val('970013');
-	//$('#zoek').click();
+	$('#invoer').val('970013');
+	$('#zoek').click();
+	
+	$("#zoek").click(function(){
+	$("#block-weather-info-user-custom").fadeOut("fast");
+		$.ajax({
+			type: "GET",
+			url: "proxy.php?url=http%3A%2F%2Fweather.yahooapis.com%2Fforecastrss%3Fw%3D" + $("#invoer").val() + "%26u%3Dc",
+			//url: "proxy.php?url=http%3A%2F%2Fcoap.me%2Fcoap%3A%2F%2F[2001%3A6a8%3A1d80%3A200%3A%3A2]%2Fobs,
+			//url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
+			dataType: "xml",
+			success: parseXml
+		});
+	});
 });
 
-$(document).on('click', '#zoek', function(){
-	$("#block-weather-info-user-custom").fadeOut("fast");
-    $.ajax({
-        type: "GET",
-        url: "proxy.php?url=http%3A%2F%2Fweather.yahooapis.com%2Fforecastrss%3Fw%3D" + $("#invoer").val() + "%26u%3Dc",
-		//url: "proxy.php?url=http%3A%2F%2Fcoap.me%2Fcoap%3A%2F%2F[2001%3A6a8%3A1d80%3A200%3A%3A2]%2Fobs,
-		//url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
-        dataType: "xml",
-        success: parseXml
-     });
-});
+
 
 function parseXml(xml)
 {
 	$("#weerblok").empty();
 	 $("#weerblok").append($(xml).find("description").text());
   $("#block-weather-info-user-custom").fadeIn("slow");
-}
+};
+
+})(jQuery);
