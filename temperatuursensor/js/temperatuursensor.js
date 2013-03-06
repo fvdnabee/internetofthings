75f5c7b7c99cb08126249aed4573fedbdf53c212
unknown <wrightkobe@hotmail.com>
2013-03-06 19:47:07 +0100

    Long polling toegevoegd --> gebruiker moet nu niet meer refreshen, er wordt automatisch om de 2 seconden gepolld. Er wordt nu ook een nieuwe waarde opgehaald als de max age verstreken is indien de gebruiker dat wenst (a;d.h.v. een checkbox)

--- temperatuursensor/js/temperatuursensor.js
+++ temperatuursensor/js/temperatuursensor.js
@@ -22,10 +22,46 @@ function parseHtml(html)
   $("#block-temperatuursensor-user-custom").fadeIn("slow");
 }
 
-Drupal.Nodejs.callbacks.waardeOntvangen = {
-  callback: function (message) {
-    Drupal.nodejs_ajax.runCommands(message);
+// Drupal.Nodejs.callbacks.waardeOntvangen = {
+  // callback: function (message) {
+    // Drupal.nodejs_ajax.runCommands(message);
+  // }
+// };
+
+Drupal.behaviors.temperatuursensor = {
+  attach: function(context) {
+		var refreshId = setInterval(function(){
+			$.ajax({
+				type: "GET",
+				url: "/temperatuur/ajax",
+				dataType: "html",
+				success: tempReceived
+			});
+		}, 2000);
   }
 };
+
+function tempReceived(html){
+	var regex = /<tr><td>(\d+)<\/td><td>(.*)<\/td><td>(.*)<\/td><td>(.*)<\/td><\/tr>/;
+	var matches = regex.exec(html);
+	console.log('nieuwe hid: ' + matches[1] + ', oude hid: ' + $('#hid1').html());
+	if(matches[1] > $('#hid1').html()){
+		$('#historytable').hide();
+		$('#temperatuur').html(matches[2]);
+		$('#max_age').html(matches[3]);
+		for (var i = 5; i >= 2; i--){
+			var nr = i-1;
+			$('#hid' + i).html($('#hid' + nr).html());
+			$('#temperature' + i).html($('#temperature' + nr).html());
+			$('#max_age' + i).html($('#max_age' + nr).html());
+			$('#timestamp' + i).html($('#timestamp' + nr).html());
+		}
+		$('#hid1').html(matches[1]);
+		$('#temperature1').html(matches[2]);
+		$('#max_age1').html(matches[3]);
+		$('#timestamp1').html(matches[4]);
+		$('#historytable').fadeIn('slow');
+	}
+}
 
 })(jQuery);
