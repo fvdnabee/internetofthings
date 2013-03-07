63ad83c9789aec8cb3d638057c039a6131c64dda
unknown <wrightkobe@hotmail.com>
2013-03-07 16:14:16 +0100

    extra module die huidig aantal uren toont

--- temperatuursensor/js/temperatuursensor.js
+++ temperatuursensor/js/temperatuursensor.js
@@ -1,32 +1,8 @@
 (function ($) {
 
 $(document).ready(function(){
-	$('#invoer').val('970013');
-	$('#zoek').click();
-	
-	$('#zoek').click(function(){
-		$("#block-temperatuursensor-user-custom").fadeOut("fast");
-		$.ajax({
-			type: "GET",
-			url: "proxy.php?url=http://coap.me/coap://[2001:6a8:1d80:200::2]/obs",
-			dataType: "html",
-			success: parseHtml
-		});
-	});
+	$('#block-temperatuursensor-user-custom').append('<img id="loading" src="sites/all/modules/custom/temperatuursensor/images/ajax-loader.gif" />');
 });
-
-function parseHtml(html)
-{
-	$("#temperatuurblok").empty();
-	 $("#temperatuurblok").append($(html).find("span").text());
-  $("#block-temperatuursensor-user-custom").fadeIn("slow");
-}
-
-// Drupal.Nodejs.callbacks.waardeOntvangen = {
-  // callback: function (message) {
-    // Drupal.nodejs_ajax.runCommands(message);
-  // }
-// };
 
 Drupal.behaviors.temperatuursensor = {
   attach: function(context) {
@@ -46,6 +22,7 @@ function tempReceived(html){
 	var matches = regex.exec(html);
 	console.log('nieuwe hid: ' + matches[1] + ', oude hid: ' + $('#hid1').html());
 	if(matches[1] > $('#hid1').html()){
+		$("#loading").fadeOut('slow');
 		$('#historytable').hide();
 		$('#temperatuur').html(matches[2]);
 		$('#max_age').html(matches[3]);
