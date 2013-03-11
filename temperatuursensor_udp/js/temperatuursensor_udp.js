2ea9a3296c2311b5b23f7103890d0c8cab21dba8
unknown <wrightkobe@hotmail.com>
2013-03-11 22:04:50 +0100

    module toegevoegd die hetzelfde als de temperatuurmodule realiseert maar nu de echte observe behandelt met coap berichten --> werkt nog niet

---
+++ temperatuursensor_udp/js/temperatuursensor_udp.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+Drupal.behaviors.temperatuursensor_udp = {
+  attach: function(context) {
+		var refreshId = setInterval(function(){
+			$.ajax({
+				type: "GET",
+				url: "/observe/ajax",
+				dataType: "text",
+				success: tempReceived
+			});
+		}, 2000);
+  }
+};
+
+function tempReceived(html){
+	var regex = /<tr><td>(\d+)<\/td><td>(.*)<\/td><\/tr>/;
+	var matches = regex.exec(html);
+	console.log('nieuwe hid: ' + matches[1] + ', oude hid: ' + $('#hid_udp1').html());
+	if(matches[1] > $('#hid1').html()){
+		$('#historytable_udp').hide();
+		for (var i = 5; i >= 2; i--){
+			var nr = i-1;
+			$('#hid_udp' + i).html($('#hid_udp' + nr).html());
+			$('#response_udp' + i).html($('#response_udp' + nr).html());
+		}
+		$('#hid1_udp').html(matches[1]);
+		$('#response_udp1').html(matches[2]);
+		$('#historytable_udp').fadeIn('slow');
+	}
+}
+
+})(jQuery);
