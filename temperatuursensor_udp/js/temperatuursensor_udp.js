e526f2125fc4eddb86765834057907189f8d5c4d
unknown <wrightkobe@hotmail.com>
2013-03-13 21:05:37 +0100

    temperatuursmodule met udp-sockets kan nu echte observe uitvoeren

--- temperatuursensor_udp/js/temperatuursensor_udp.js
+++ temperatuursensor_udp/js/temperatuursensor_udp.js
@@ -16,15 +16,18 @@ Drupal.behaviors.temperatuursensor_udp = {
 function tempReceived(html){
 	var regex = /<tr><td>(\d+)<\/td><td>(.*)<\/td><\/tr>/;
 	var matches = regex.exec(html);
+	console.log(html);
 	console.log('nieuwe hid: ' + matches[1] + ', oude hid: ' + $('#hid_udp1').html());
-	if(matches[1] > $('#hid1').html()){
+	var nieuw = parseInt(matches[1]);
+	var oud = parseInt($('#hid1').html());
+	if(matches[1] > $('#hid_udp1').html()){
 		$('#historytable_udp').hide();
 		for (var i = 5; i >= 2; i--){
 			var nr = i-1;
 			$('#hid_udp' + i).html($('#hid_udp' + nr).html());
 			$('#response_udp' + i).html($('#response_udp' + nr).html());
 		}
-		$('#hid1_udp').html(matches[1]);
+		$('#hid_udp1').html(matches[1]);
 		$('#response_udp1').html(matches[2]);
 		$('#historytable_udp').fadeIn('slow');
 	}
