c971a72fa6c2983f756f19f2466729663c7763d9
Kobe Wright <wrightkobe@hotmail.com>
2013-04-10 08:24:21 +0200

    Aanvullingen aan temperatuurmodule en CoAP library, GET, PUT, POST en DELETE toegevoegd

--- temperatuursensor_met_coap_library/js/temperatuursensor_met_coap_library.js
+++ temperatuursensor_met_coap_library/js/temperatuursensor_met_coap_library.js
@@ -21,14 +21,14 @@ Drupal.behaviors.temperatuursensor_udp = {
 };
 
 function tempReceived(html){
-	var regex = /<error>(.*)<\/error><responded>(.*)<\/responded><get_response>(.*)<\/get_response><tr><td>(\d+)<\/td><td>(.*)<\/td><td>(.*)<\/td><td>(.*)<\/td><\/tr>/;
+	var regex = /<error>(.*)<\/error><responded>(.*)<\/responded><get_response>(.*)<\/get_response><method>(.*)<\/method><response_type>(.*)<\/response_type><tr><td>(\d+)<\/td><td>(.*)<\/td><td>(.*)<\/td><td>(.*)<\/td><\/tr>/;
 	var matches = regex.exec(html);
 	console.log('input ajax call: ' + html);
 	if(matches && matches[1] == 'none'){
 		$('#error').hide();
 		$('#errorimg').attr('style', 'visibility:hidden;');
-		console.log('nieuwe hid: ' + matches[4] + ', oude hid: ' + $('#hid1').html());
-		var nieuw = parseInt(matches[4]);
+		console.log('nieuwe hid: ' + matches[6] + ', oude hid: ' + $('#hid1').html());
+		var nieuw = parseInt(matches[6]);
 		var oud = parseInt($('#hid1').html());
 		if(nieuw > oud){
 			$('#error').html('');
@@ -40,21 +40,24 @@ function tempReceived(html){
 				$('#max_age' + i).html($('#max_age' + nr).html());
 				$('#timestamp' + i).html($('#timestamp' + nr).html());
 			}
-			$('#hid1').html(matches[4]);
-			$('#temperatuur1').html(matches[5]);
-			$('#max_age1').html(matches[6]);
-			$('#timestamp1').html(matches[7]);
+			$('#hid1').html(matches[6]);
+			$('#temperatuur1').html(matches[7]);
+			$('#max_age1').html(matches[8]);
+			$('#timestamp1').html(matches[9]);
 			$('#historytable').fadeIn('slow');
 			drawChart();
 		}
-		if(matches[2] == 'yes'){
-			$('#get_response').hide();
-			$('#get_response').html("Response: " + matches[3]);
-			$('#get_response').fadeIn('slow');
+		if(matches[2] == 1){
+			$('#response').hide();
+			$('#response_type').hide();
+			$('#response').html("Response: " + matches[3]);
+			$('#response_type').html("Response type: " + matches[5]);
+			$('#response').fadeIn('slow');
+			$('#response_type').fadeIn('slow');
 		}
 	}
 	else{
-		var regex = /<error>(.*)<\/error><responded>(.*)<\/responded><get_response>(.*)<\/get_response>/;
+		var regex = /<error>(.*)<\/error><responded>(.*)<\/responded><get_response>(.*)<\/get_response><method>(.*)<\/method><response_type>(.*)<\/response_type>/;
 		var matches = regex.exec(html);
 		if(matches){
 			if(matches[1] == 'unreachable'){
@@ -86,9 +89,12 @@ function tempReceived(html){
 				$('#error').html('Er werd een ongeldige URI opgegeven');
 			}
 				if(matches[2] == 'yes'){
-				$('#get_response').hide();
-				$('#get_response').html("Response: " + matches[3]);
-				$('#get_response').fadeIn('slow');
+				$('#response').hide();
+				$('#response_type').hide();
+				$('#response').html("Response: " + matches[3]);
+				$('#response_type').html("Response type: " + matches[5]);
+				$('#response').fadeIn('slow');
+				$('#response_type').fadeIn('slow');
 			}
 		}
 		else{
