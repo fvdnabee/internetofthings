9fbb12367763598a1562eb267749ac09af70dac9
Kobe Wright <wrightkobe@hotmail.com>
2013-05-14 16:14:18 +0200

    aanpassen aan nieuw veld

--- coap_resource/templates/node--coap_resource.tpl.php
+++ coap_resource/templates/node--coap_resource.tpl.php
@@ -1,62 +1,62 @@
-<div class = 'coap_resource' id = "<?php echo $content['field_resource_uri']['#items'][0]['uri']; ?>" style="padding:5px;border:2px solid;border-radius:10px;box-shadow:10px 10px 5px #888888;" >
-	<h1 class = "uri" style = "text-align:center" ><?php echo $content['field_resource_uri']['#items'][0]['uri']; ?></h1>
+<div class = 'coap_resource' id = "<?php echo $content['field_resource_uri']['#items'][0]['value']; ?>" style="padding:5px;border:2px solid;border-radius:10px;box-shadow:10px 10px 5px #888888;" >
+	<h1 class = "uri" style = "text-align:center" ><?php echo $content['field_resource_uri']['#items'][0]['value']; ?></h1>
 	<div class = "header" align = "center" >
-		<input type = "text" class = "POLLING_INVOER" size = "2" id = "<?php echo "polling_invoer_" . $content['field_resource_uri']['#items'][0]['uri'] ?>" 
+		<input type = "text" class = "POLLING_INVOER" size = "2" id = "<?php echo "polling_invoer_" . $content['field_resource_uri']['#items'][0]['value'] ?>" 
 					value = "<?php 	global $user;
 									$result = db_select('coap_resource_users', 'users')
 									->fields('users', array('uid', 'uri', 'polling_interval'))
 									->condition('uid', $user->uid, '=')
 									->condition('nid', $node->nid, '=')
-									->condition('uri', $content['field_resource_uri']['#items'][0]['uri'], '=')
+									->condition('uri', $content['field_resource_uri']['#items'][0]['value'], '=')
 									->execute();
 									foreach($result as $record){
 										echo $record->polling_interval;
 									} ?>" />
-		<input type = "button" id = "<?php echo "btn_polling_" . $content['field_resource_uri']['#items'][0]['uri'] ?>" 
+		<input type = "button" id = "<?php echo "btn_polling_" . $content['field_resource_uri']['#items'][0]['value'] ?>" 
 					class = "POLLING_BUTTON form-submit" value = "Change polling interval" />
 	</div>
 	<div class = 'GET'>
-		<input type = "button" id = "<?php echo "btn_GET_" . $content['field_resource_uri']['#items'][0]['uri'] ?>" 
+		<input type = "button" id = "<?php echo "btn_GET_" . $content['field_resource_uri']['#items'][0]['value'] ?>" 
 				class = "REQUEST_BUTTON form-submit" value = "GET" />
-		<label id = "<?php echo 'lbl_GET_' . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<label id = "<?php echo 'lbl_GET_' . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				style="display:inline" >Perform a GET request</label>
 	</div>
 	<div class = 'PUT'>
-		<input type = "button" id = "<?php echo "btn_PUT_" . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<input type = "button" id = "<?php echo "btn_PUT_" . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				class = "REQUEST_BUTTON form-submit" value = "PUT" />
-		<input 	id = "<?php echo 'input_PUT_' . $content['field_resource_uri']['#items'][0]['uri'] ?>" 
+		<input 	id = "<?php echo 'input_PUT_' . $content['field_resource_uri']['#items'][0]['value'] ?>" 
 				type = "text"
 				style="display:inline"
 				class = "PUT_INPUT" >
-		<label 	id = "<?php echo 'lbl_PUT_' . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<label 	id = "<?php echo 'lbl_PUT_' . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				style = "display:inline" >Perform a PUT request with given value</label>
 	</div>
 	<div class = 'POST'>
-		<input type = "button" id = "<?php echo "btn_POST_" . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<input type = "button" id = "<?php echo "btn_POST_" . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				class = "REQUEST_BUTTON form-submit" value = "POST" />
-		<input 	id = "<?php echo 'input_POST_' . $content['field_resource_uri']['#items'][0]['uri'] ?>" 
+		<input 	id = "<?php echo 'input_POST_' . $content['field_resource_uri']['#items'][0]['value'] ?>" 
 				type = "text"
 				style="display:inline"
 				class = "POST_INPUT" >
-		<label 	id = "<?php echo 'lbl_POST_' . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<label 	id = "<?php echo 'lbl_POST_' . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				style = "display:inline" >Perform a POST request with given value</label>
 	</div>
 	<div class = 'DELETE'>
-		<input type = "button" id = "<?php echo "btn_DELETE_" . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<input type = "button" id = "<?php echo "btn_DELETE_" . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				class = "REQUEST_BUTTON form-submit" value = "DELETE" />
-		<label 	id = "<?php echo 'lbl_DELETE_' . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<label 	id = "<?php echo 'lbl_DELETE_' . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				style = "display:inline" >Perform a DELETE request</label>
 	<div class = 'OBSERVE' align = "center" >
-		<input type = "button" id = "<?php echo "btn_OBSERVE_" . $content['field_resource_uri']['#items'][0]['uri'] ?>"
+		<input type = "button" id = "<?php echo "btn_OBSERVE_" . $content['field_resource_uri']['#items'][0]['value'] ?>"
 				class = "OBSERVE_BUTTON form-submit" value = "<?php $result = db_select('coap_resource_users', 'users')
 																	->fields('users', array('uid', 'uri'))
 																	->condition('uid', $user->uid, '=')
 																	->condition('device', 0, '=')
-																	->condition('uri', $content['field_resource_uri']['#items'][0]['uri'], '=')
+																	->condition('uri', $content['field_resource_uri']['#items'][0]['value'], '=')
 																	->condition('observe', 1, '=')
 																	->execute();
 																	echo ($result->rowCount() ? 'Stop' : 'Start'); ?> Observing" />
-		<label id = "<?php echo "lbl_OBSERVE_" . $content['field_resource_uri']['#items'][0]['uri'] ?>" style = "text-align:center" >Observe input comes here</label>
+		<label id = "<?php echo "lbl_OBSERVE_" . $content['field_resource_uri']['#items'][0]['value'] ?>" style = "text-align:center" >Observe input comes here</label>
 	</div>
 	<div class = "history" >
 		<h1>History</h1>
@@ -84,7 +84,7 @@
 					$nr_entrys = 0;
 					$result = db_select('coap_resource_values', 'coap_values')
 						->fields('coap_values', array('timestamp', 'parsed_value'))
-						->condition('uri', $content['field_resource_uri']['#items'][0]['uri'], '=')
+						->condition('uri', $content['field_resource_uri']['#items'][0]['value'], '=')
 						->condition('uid', $user->uid, '=')
 						->range(0, 10)
 						->orderBy('hid', 'DESC')
